896cd6c7c8ac89725ddc8290fa2d0f4993bcb401
BoonMode <69335910+BoonMode@users.noreply.github.com>
2020-08-19 17:46:44 +0800

    Basic Box Select
    Selection box using a singleton pattern and a basic collider detection component.

---
+++ SelectionBox.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionBox : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Selection.Instance.CheckAndAdd(other.gameObject);
+    }
+}
